75abdecb58e0d5c7010ad892b86c549ab262a9f1
gonzo1776 <mgonzo1785@gmail.com>
2019-03-14 20:20:02 +0000

    added more info page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,7 +13,12 @@ before_action :authenticate_user!, only: [:new, :create]
     redirect_to root_path
   end
 
-  private
+def show 
+  @place = Place.find(params[:id])
+end
+
+
+private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
