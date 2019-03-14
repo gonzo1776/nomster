ac59edcc0be4421ab042c940ab1bcf4ada1cf513
gonzo1776 <mgonzo1785@gmail.com>
2019-03-14 23:20:05 +0000

    updated place data and root redirect

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,12 @@ end
     @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
 private
 
   def place_params
