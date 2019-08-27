954fdbc01141a32124b69b166bec826babd12e0d
gonzo1776 <mgonzo1785@gmail.com>
2019-08-27 03:43:15 +0000

    changed carousel pics

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do
-default_url_options Rails.application.config.action_mailer.default_url_options
+  Rails.application.routes.draw do
+# default_url_options Rails.application.config.action_mailer.default_url_options
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
